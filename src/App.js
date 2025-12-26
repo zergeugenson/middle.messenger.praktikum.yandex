623520de2e41fe320295b7e272fbd0262556a159
Khromov <hromov@price.ru>
2025-12-26 11:14:33 +0100

    small fixes

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ import { mockCreditionals } from './mock/mockData.js';
 export default class App {
 	constructor() {
 		this.state = {
-			currentPage: 'profilePage',
+			currentPage: 'chatPage',
 			creditionals: mockCreditionals,
 			creditionalsFieldLabels: creditionalsFieldLabels,
 			errorCode: 502,
