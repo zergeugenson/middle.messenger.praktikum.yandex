b922e8b823b2e2086eceba0767a2e90df9138efe
Khromov <hromov@price.ru>
2026-01-20 12:32:46 +0100

    Add linters, ts, configs

---
+++ eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  overrides: [
+    {
+      files: ['*.js', '*.cjs'],
+      parserOptions: {
+        project: './tsconfig.node.json',
+      },
+    },
+  ],
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    '@typescript-eslint/no-explicit-any': 'off', 
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+  },
+};
