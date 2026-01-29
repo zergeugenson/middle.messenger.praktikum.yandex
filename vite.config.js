51a5ea512cb4d38b510c4bc825be7fceed2ff348
Khromov <hromov@price.ru>
2026-01-29 09:45:23 +0100

    linters after pull request

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,23 @@ import path from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-	plugins: [tsconfigPaths()],
-	build: {
-		rollupOptions: {
-			input: {
-				main: resolve(__dirname, 'index.html'),
-			},
-		},
-	},
-	css: {
-		preprocessorOptions: {
-			scss: {},
-		},
-	},
-	publicDir: 'public',
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
+  plugins: [tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {},
+    },
+  },
+  publicDir: 'public',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
