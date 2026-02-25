ac519885879c4c28353b9d12aea6424ea7112cae
Khromov <hromov@price.ru>
2026-02-25 13:07:41 +0100

    -- any

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,22 +1,22 @@
 import UserApi from '@/api/User';
-
+import type { User } from '@/types';
 const userApi = new UserApi();
 
-const changeUserProfile = async (data: any): Promise<void> => {
-  const user = await userApi.profile(data);
+const changeUserProfile = async (data: User): Promise<unknown> => {
+  const user = await userApi.profile(data) as User;
   window.store.set({ user });
   return user;
 };
 
-const changeUserAvatar = async (file: File): Promise<void> => {
+const changeUserAvatar = async ({ file }: { file: File }): Promise<unknown> => {
   const res = new FormData();
   res.append('avatar', file);
-  const user = await userApi.avatar(res);
+  const user = await userApi.avatar(res) as User;
   window.store.set({ user });
   return user;
 };
 
-const changeUserPassword = async (data: any): Promise<void> => {
+const changeUserPassword = async (data: { oldPassword: string, newPassword: string }): Promise<unknown> => {
   return userApi.password(data);
 };
 
